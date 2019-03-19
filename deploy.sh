4ef1c349286542f2b2a616571a6a74aa251fd2a3
liuheng <liuheng@yidian-inc.com>
2019-03-19 17:01:42 +0800

    add version to image

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ pwd
 ls
 # docker stop yapi-roy-cook-com
 # docker rm yapi-roy-cook-com
-# docker rmi roycook/yapi-roy-cook-com
+# docker rmi roycook/yapi-roy-cook-com:1.5.7
 docker build -t roycook/yapi-roy-cook-com:1.5.7 .
 docker run -d --name yapi-roy-cook-com -p 8002:8002 roycook/yapi-roy-cook-com
