5a9feb81ed84e1e2b13f8392d2833e1f5a873f35
liuheng <liuheng@yidian-inc.com>
2019-03-19 16:36:15 +0800

    change image name

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ pwd
 ls
 # docker stop yapi-roy-cook-com
 # docker rm yapi-roy-cook-com
-# docker rmi node/yapi-roy-cook-com
-docker build -t node/yapi-roy-cook-com .
-docker run -d --name yapi-roy-cook-com -p 8002:8002 node/yapi-roy-cook-com
+# docker rmi yapi-roy-cook-com
+docker build -t yapi-roy-cook-com .
+docker run -d --name yapi-roy-cook-com -p 8002:8002 yapi-roy-cook-com
