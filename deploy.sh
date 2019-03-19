80d71ea88368fe6226d8dbb8f9ac305040199bfc
liuheng <liuheng@yidian-inc.com>
2019-03-19 18:35:05 +0800

    change default port

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 # docker rm yapi-roy-cook-com
 # docker rmi roycook/yapi-roy-cook-com:1.5.7
 docker build -t roycook/yapi-roy-cook-com:1.5.7 .
-docker run -d --name yapi-roy-cook-com -p 8002:8002 roycook/yapi-roy-cook-com:1.5.7
+docker run -d --name yapi-roy-cook-com -p 8082:8082 roycook/yapi-roy-cook-com:1.5.7
