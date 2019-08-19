26b03edccc36c27ba04dd9cf47671eae02d6f37f
liuheng.roy <liuheng.roy@bytedance.com>
2019-08-19 14:31:30 +0800

    finish updrate

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # yapi-roy-cook-com
+
+### 项目升级yapi server
+### 在浏览器打开 http://0.0.0.0:9090 访问 填写内容如config.json
+### sudo rm -rf init.lock
+### node vendors/server/install.js & node vendors/server/app.js测试
+
+### 测试成功 清空yapi数据库
+
+### docker build -t yapi-roy-cook-com:1.8.1 .
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 # docker stop yapi-roy-cook-com
 # docker rm yapi-roy-cook-com
-# docker rmi roycook/yapi-roy-cook-com:1.5.7
-docker build -t roycook/yapi-roy-cook-com:1.5.7 .
-docker run -d --name yapi-roy-cook-com -p 8082:8082 roycook/yapi-roy-cook-com:1.5.7
+# docker rmi roycook/yapi-roy-cook-com:1.8.1
+docker build -t roycook/yapi-roy-cook-com:1.8.1 .
+docker run -d --name yapi-roy-cook-com -p 8082:8082 roycook/yapi-roy-cook-com:1.8.1
